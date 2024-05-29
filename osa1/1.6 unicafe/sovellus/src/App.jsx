ed3aad5875c942fa9f2f69ff9aa6fa2213c5d2a8
Kasper Kivistö <kivisto.kasper@gmail.com>
2024-05-29 16:59:58 +0300

    Tehtävä 1.6

--- osa1/1.6 unicafe/sovellus/src/App.jsx
+++ osa1/1.6 unicafe/sovellus/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -8,7 +16,13 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1> give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="Good"/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
+      <Button handleClick={() => setBad(bad + 1)} text="Bad"/>
+      <div>Good: {good}</div>
+      <div>Neutral: {neutral}</div>
+      <div>Bad: {bad}</div>
     </div>
   )
 }
