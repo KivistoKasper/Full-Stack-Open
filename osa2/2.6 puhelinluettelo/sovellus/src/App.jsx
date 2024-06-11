d9aa12f3480ff7a3d82001213123e5517aa00b4f
Kasper Kivistö <kivisto.kasper@gmail.com>
2024-06-11 14:01:09 +0300

    Tehtävä 2.6

---
+++ osa2/2.6 puhelinluettelo/sovellus/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+
+const Part = (props) => {
+  //console.log(props.info)
+  return (
+    <div>
+      <p>{props.info.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+const addNumber = event => {
+  event.preventDefault();
+  //console.log('click ', event.target.value);
+  const nameObject = {
+    name: newName,
+  }
+
+  setPersons(persons.concat(nameObject));
+  setNewName('');
+}
+
+const handleInputChange = event => {
+  //console.log('inout change ', event.target.value);
+  setNewName(event.target.value);
+}
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person =>
+          <Part key={person.name} info={person}/>
+        )}
+      </div>
+    </div>
+  )
+
+}
+
+export default App
