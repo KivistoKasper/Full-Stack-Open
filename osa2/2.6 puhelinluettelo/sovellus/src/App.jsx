c9aa38551f7f8c0c0e4dbe4fb8598cbad6c9a0b5
Kasper Kivistö <kivisto.kasper@gmail.com>
2024-06-11 16:48:19 +0300

    Tehtävä 2.11

--- osa2/2.6 puhelinluettelo/sovellus/src/App.jsx
+++ osa2/2.6 puhelinluettelo/sovellus/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const Part = (props) => {
@@ -47,15 +48,26 @@ const Persons = ({personsToShow}) => {
 }
 
 const App = () => {
+  /*
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  ])
+    */ 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+// get data from server
+useEffect(() => {
+  axios.get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+}, [])
 
 
 const addPerson = event => {
