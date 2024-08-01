29f944db9830c846421a24b709433c15cecd42fc
Kasper Kivistö <kivisto.kasper@gmail.com>
2024-08-01 15:55:01 +0300

    Tehtävä 4.1. Toimiva npm-projekti

---
+++ osa4/4.1 blogilista/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const Blog = require('./models/blog')
+
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  console.log('request: ', request.body)
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+// handling of non-existent addresses
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
