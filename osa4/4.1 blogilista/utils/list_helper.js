04fc0f5a5a1c6436cf5e90da77ec48930b4ed902
Kasper Kivistö <kivisto.kasper@gmail.com>
2024-08-02 16:33:01 +0300

    Tehtävä 4.4. TotalLikes testit

--- osa4/4.1 blogilista/utils/list_helper.js
+++ osa4/4.1 blogilista/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+    const initialValue = 0
+    const sumOfLikes = blogs.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.likes, 
+        initialValue
+    )
+    return sumOfLikes
+}
   
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
