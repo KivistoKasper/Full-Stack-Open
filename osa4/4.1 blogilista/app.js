d4fe4d6b58125eed2525ee01d79b47bf8d0f5729
Kasper Kivistö <kivisto.kasper@gmail.com>
2024-08-06 13:41:05 +0300

    Tehtävä 4.15. käyttäjien lisääminen ja hakeminen sekä lisäämisen testejä

--- osa4/4.1 blogilista/app.js
+++ osa4/4.1 blogilista/app.js
@@ -3,7 +3,9 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -31,6 +33,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
