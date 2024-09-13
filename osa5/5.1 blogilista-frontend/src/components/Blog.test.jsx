49443fa24cb2cc5c24fe4cf30bb3f3a7ebf7aae0
Kasper Kivistö <kivisto.kasper@gmail.com>
2024-09-13 16:17:35 +0300

    Tehtävä 5.14. Testi: napin painalluksen jälkeen näytetään myös url, liket ja käyttäjä

--- osa5/5.1 blogilista-frontend/src/components/Blog.test.jsx
+++ osa5/5.1 blogilista-frontend/src/components/Blog.test.jsx
@@ -1,18 +1,24 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
-import { beforeEach, describe } from 'vitest'
+import { beforeEach, describe, expect } from 'vitest'
 
 describe('<NewBlogForm />', () => {
 
   const blog = {
     title: 'Component testing is done with react-testing-library',
     author: 'Component testing author',
+    url: 'www.componenttesting.com',
+    likes: 3,
     user: {
-      username: 'Component testing user'
+      username: 'Component testing user',
+      name: 'user\'s name',
+      id: 1234
     }
   }
   const user = {
-    username: 'Component testing user'
+    username: 'Component testing user',
+    id: 1234
   }
 
   let container
@@ -20,18 +26,28 @@ describe('<NewBlogForm />', () => {
     container = render(<Blog blog={blog} user={user}/>).container
   })
 
-  test('renders content', () => {
+  test('renders content, but not togglable content', () => {
     const div = container.querySelector('.basicContent')
-    expect(div).toHaveTextContent(
-      'Component testing is done with react-testing-library'
-    )
+
+    expect(div).toHaveTextContent('Component testing is done with react-testing-library')
+    expect(div).not.toHaveTextContent('www.componenttesting.com')
+    expect(div).not.toHaveTextContent('likes')
+    expect(div).not.toHaveTextContent('user\'s name')
   })
 
-  test('at start the children are not displayed', () => {
+  test('renders togglable content after button press', async () => {
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
     const div = container.querySelector('.togglableContent')
     //screen.debug(div)
-    expect(div).toHaveTextContent(
-      'Component testing is done with react-testing-library'
-    )
+
+    expect(div).not.toHaveStyle('display: none')
+    expect(div).toHaveTextContent('Component testing is done with react-testing-library')
+    expect(div).toHaveTextContent('www.componenttesting.com')
+    expect(div).toHaveTextContent('likes')
+    expect(div).toHaveTextContent('user\'s name')
   })
 })
