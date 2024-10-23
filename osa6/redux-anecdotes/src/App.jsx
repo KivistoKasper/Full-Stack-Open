39a0dadece481f105ffe24a97dc4048aa51b1627
Kasper Kivistö <kivisto.kasper@gmail.com>
2024-10-23 16:58:16 +0300

    Tehtävä 6.8. Anekdootti listan ja formin siiräminen omiin komponentteihinsa

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -1,34 +1,14 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { createAnecdote, voteAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
-//import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
   
-  
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.newAnecdote.value
-    console.log('add anecdote ', content)
-    dispatch(createAnecdote(content))
-    event.target.newAnecdote.value = ''
-  }
 
   return (
     <div>
       <h2>Anecdotes</h2>
+      <AnecdoteList />
       <AnecdoteForm />
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input 
-          type='text'
-          name='newAnecdote'
-          /></div>
-        <button type='submit'>create</button>
-      </form>
     </div>
   )
 }
--- osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,30 +1,27 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteForm = () => {
-  const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
-  const vote = (id) => {
-    console.log('vote', id)
-    dispatch(voteAnecdote(id))
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.newAnecdote.value
+    console.log('add anecdote ', content)
+    dispatch(createAnecdote(content))
+    event.target.newAnecdote.value = ''
   }
 
   return (
     <div>
-      {anecdotes.sort(
-        (a,b) => a.votes < b.votes ? 1 : -1
-      ).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input 
+          type='text'
+          name='newAnecdote'
+          /></div>
+        <button type='submit'>create</button>
+      </form>
     </div>
   )
 }
