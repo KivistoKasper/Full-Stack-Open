32b4a94789a29767a2485ca0918695b279c2eb9d
Kivistö Kasper <kivisto.kasper@protonmail.com>
2024-11-23 15:10:22 +0200

    Tehtävä 7.4. useField käyttö uuden anekdootin luomisessa

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router,
         Link,
         useParams,
         useNavigate} from 'react-router-dom'
+import { useField } from './hooks'
 
 const Notification = ({msg}) => {
   const style = {
@@ -86,17 +87,17 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
@@ -108,15 +109,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content}/>
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author}/>
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info}/>
         </div>
         <button>create</button>
       </form>
@@ -147,7 +148,7 @@ const App = () => {
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000)
     setAnecdotes(anecdotes.concat(anecdote))
-    
+
     setNotification(`a new anecdote ${anecdote.content} created!`)
     setTimeout(() => {
       setNotification(null)
